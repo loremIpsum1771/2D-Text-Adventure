390814f823afd92ccaab02da1f3f5c6c88f1399a
loremIpsum1771 <loremIpsum1771@users.noreply.github.com>
2015-08-08 12:46:03 -0400

    Update inventory.h

--- inventory.h
+++ inventory.h
@@ -1,65 +1,69 @@
-#ifndef INVENTORY_H
-#define INVENTORY_H
+	#ifndef INVENTORY_H
+	#define INVENTORY_H
 
-#include<vector>
-#include<string>
-#include<map>
-#include "actors.h"
+	#include<vector>
+	#include<string>
+	#include<map>
+	#include "actors.h"
 
-using namespace std;
-namespace textAdventure{
-	class mainActor;
-	class enemy;
-	/*class weapon{
-	int damagePoints;
-	string weaponName;
-	public:
-	virtual void useWeapon(mainActor& theActor, enemy& theEnemy);
-	virtual int getWeaponStrength();
-	virtual string getName();
-	};
+	using namespace std;
+	namespace textAdventure{
+		class mainActor;
+		class enemy;
+		class weapon{
+		int damagePoints;
+		string weaponName;
+		public:
+			weapon();
+		virtual void useWeapon(mainActor& theActor, enemy& theEnemy);
+		virtual int getWeaponStrength();
+		virtual string getName();
+		};
 
-	class gun : public weapon{
-	int damagePoints =0;
-	public:
-	gun();
-	};
+		/*class gun : public weapon{
+		int damagePoints =0;
+		public:
+		gun();
+		};
 
-	class knife : public weapon{
-	int damagePoints = 0;
-	public:
-	knife();
-	};*/
-	class healthPack{
-		int hpStrength = 0;
-	public:
-		healthPack(int hp);
-		int getHealthValue();
-		void addHealth(mainActor anActor);
-	};
+		class knife : public weapon{
+		int damagePoints = 0;
+		public:
+		knife();
+		};*/
 
-	//class hints{
+		class healthPack{
+			int hpStrength = 0;
+		public:
+			healthPack(int hp);
+			int getHealthValue();
+			void addHealth(mainActor anActor);
+		};
 
-
-	//};
-
-	class inventory{
-		//typedef std::map<std::string, weapon*> WeaponsContainer;
-		//typedef std::pair<std::string, weapon*> MyPair;
-		//WeaponsContainer weapons;
-		/*map<string, weapon* > weaponsContainer;*/
-		vector<healthPack*> healthPacks;
-		/*map<int, hints> hintsContainer;*/
+		//class hints{
 
 
-	public:
-		void pickUpHealth(healthPack* anHp);
-		/*void pickUpWeapon(weapon aWeapon);*/
-	};
+		//};
+
+		class inventory{
+			typedef std::map<std::string, weapon*> WeaponsContainer;
+			typedef std::pair<std::string, weapon*> MyPair;
+			WeaponsContainer weapons;
+			map<string, weapon* > weaponsContainer;
+			vector<healthPack*> healthPacks;
+			//map<int, hints> hintsContainer;
 
 
-}
+		public:
+			void pickUpHealth(healthPack* anHp);
+			void pickUpWeapon(weapon* aWeapon);
+			void displayWeapons();
+			void displayhealthPacks();
+		};
+
+
+	}
 
 
 
-#endif
+	#endif
