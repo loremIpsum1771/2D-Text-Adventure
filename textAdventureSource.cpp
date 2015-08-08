8d07ae1848b4b535549dba569ef99132a1f39f03
loremIpsum1771 <loremIpsum1771@users.noreply.github.com>
2015-08-08 12:41:04 -0400

    Update textAdventureSource.cpp

--- textAdventureSource.cpp
+++ textAdventureSource.cpp
@@ -16,34 +16,67 @@ void tokenizer(char str[], vector<string>& tokens, const char delim[]) {
 	char* tok = strtok(str, delim);
 	while (tok != nullptr) {
 		tokens.push_back(tok);
-		
+
 		tok = strtok(nullptr, delim);
 	}
 }
 
-void renderMap(vector<string> roomNames, vector<string> roomDescriptions){
+void renderMap(gameMap& map){
 	ifstream txtFile("adventureExposition.txt");
 	if (!txtFile) { cerr << "File not found"; return; }
 	string fileLine;
-	getline(txtFile, fileLine);
-
+	//getline(txtFile, fileLine);
+	int i = 0;
+	vector<Room*> roomPtrs;
+	vector<enemy*> enemyPtrs;
 	while (getline(txtFile, fileLine)) {
-		vector<string> roomInfo;
-		char delim[] = "|";
-		char* charStopLine = new char[fileLine.size() + 1];
-		strcpy(charStopLine, fileLine.c_str());
+		if (i < 16){
+			vector<string> roomInfo;
+			vector<string> enemyInfo;
+			char delim[] = "|";
+			char delim2[] = "$";
+			char* charStopLine = new char[fileLine.size() + 1];
+			strcpy(charStopLine, fileLine.c_str());
 
-		tokenizer(charStopLine, roomInfo, delim);
-		roomNames.push_back(*(roomInfo.begin()));
-		cout << "room Names " << *(roomNames.begin()) << endl;
-		roomDescriptions.push_back(*(roomInfo.begin()+1));
-		cout << "room Descriptions " << *(roomDescriptions.begin()) << endl;
-		roomInfo.pop_back();
-		roomInfo.pop_back();
-		delete[] charStopLine;
+			tokenizer(charStopLine, roomInfo, delim);
+			tokenizer(charStopLine, enemyInfo, delim2);
+			string roomName = *(roomInfo.begin());
+			string roomDescription = *(roomInfo.begin() + 1);
+			int roomXdim = stoi(*(roomInfo.begin() + 2));
+			int roomYdim = stoi(*(roomInfo.begin() + 3));
+			Room* roomPtr = new Room(roomName, roomXdim, roomYdim);
+			//don't remember why this is here
+			//roomPtrs.push_back(roomPtr);
+			//Room* eachRoom = *(roomPtrs.begin() + i);
+			roomPtr->setRoomDesc(roomDescription);
+			
+
+			/*string enemyName = *(enemyInfo.begin());
+			int enemyHp = stoi(*(enemyInfo.begin() + 1));
+			int enemyStrength = stoi(*(enemyInfo.begin() + 2));
+			string weaponName = *(enemyInfo.begin() + 3);
+			enemy* enemyPtr = new enemy(enemyName);
+
+			knife* enemyWeapon = new knife();
+			enemyPtr->setStrength(enemyStrength);
+			enemyPtr->setHp(enemyHp);
+			enemyPtr->setWeapon(enemyWeapon);
+			roomPtr->addRoomEnemy(enemyPtr);*/
+			//enemyPtrs.push_back(enemyPtr);
+
+			map.addRoom(roomPtr);
+			/*roomNames.push_back(*(roomInfo.begin()));
+			cout << "room Names " << *(roomNames.begin()) << endl;
+			roomDescriptions.push_back(*(roomInfo.begin()+1));
+			cout << "room Descriptions " << *(roomDescriptions.begin()) << endl;*/
+
+			delete[] charStopLine;
+		}
+		i++;
 	}
-
 }
+
+
 void gameLoop(){
 	string userControl;
 
@@ -52,41 +85,11 @@ void gameLoop(){
 	cin >> cName;
 	mainActor anActor(cName);
 	gameMap map;
+	renderMap(map);
 
-	string roomName1 = "Spawn Point1";
-
-	//Room room1(roomName1,0,0);
-	Room* room1Ptr = new Room(roomName1, 0, 0);
-
-	map.addRoom(room1Ptr);
-	room1Ptr->setRoomDesc("this is spawn point 1");
-
-	string roomName2 = "Spawn Point2";
-	Room* room2Ptr = new Room(roomName2, 0, 1);
-
-	map.addRoom(room2Ptr);
-	room2Ptr->setRoomDesc("this is spawn point 2");
-
-	string roomName3 = "Spawn Point3";
-	Room* room3Ptr = new Room(roomName3, 1, 0);
-	map.addRoom(room3Ptr);
-	room3Ptr->setRoomDesc("this is spawn point 3");
-
-	string roomName4 = "Spawn Point4";
-	Room* room4Ptr = new Room(roomName4, 1, 1);
-	map.addRoom(room4Ptr);
-	room4Ptr->setRoomDesc("this is spawn point 4");
-
-	/*string roomName5 = "Spawn Point5";
-	Room* room5Ptr = new Room(roomName5, 1, 0);
-	map.addRoom(room5Ptr);
-	room5Ptr->setRoomDesc("this is spawn point 5");
-
-	string roomName6 = "Spawn Point6";
-	Room* room6Ptr = new Room(roomName6, 2, 0);
-	map.addRoom(room6Ptr);
-	room6Ptr->setRoomDesc("this is spawn point 6");*/
-	map.setCurrentPosition(0, 0);
+	map.initializeMap();
+	map.printBoard();
+	
 	while (userControl != "quit"){
 		cout << "choose a direction" << endl;
 		cin >> userControl;
@@ -95,20 +98,22 @@ void gameLoop(){
 		}
 		if (userControl == "right"){
 			map.navigateRight();
+			map.printBoard();
+			
 		}
 		if (userControl == "forward"){
 			map.navigateForward();
 		}
 		if (userControl == "back"){
+		
 			map.navigateBackward();
 		}
 	}
 }
 
 int main(){
-	vector<string> roomNames;
-	vector<string> roomDescriptions;
-	renderMap(roomNames, roomDescriptions);
-	//gameLoop();
+	
+
+	gameLoop();
 	return 0;
 }
