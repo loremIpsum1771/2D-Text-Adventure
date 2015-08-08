2a3277919ac2eda4dfe1270177e21baf8220cfc4
loremIpsum1771 <loremIpsum1771@users.noreply.github.com>
2015-08-08 12:47:13 -0400

    Update gameboard.h

--- gameboard.h
+++ gameboard.h
@@ -4,6 +4,8 @@
 #include<vector>
 #include<string>
 #include<iostream>
+#include<utility>
+#include<stack>
 #include "actors.h"
 using namespace std;
 
@@ -14,12 +16,12 @@ namespace textAdventure{
 		string roomDescription;
 		int roomRow;
 		int roomColumn;
-		vector<enemy> roomEnemies;
+		vector<enemy*> roomEnemies;
 	public:
 		Room(string& rName, int row, int column);
 		string getRoomDesc();
 		string getRoomName();
-		void addRoomEnemy(enemy& anEnemy);
+		void addRoomEnemy(enemy* anEnemy);
 		void setRoomDesc(string description);
 		pair<int, int> currentPosition();
 		
@@ -29,14 +31,16 @@ namespace textAdventure{
 	class gameMap{
 
 		//vector< vector<Room>> gameBoard;
-		Room* gameBoard[2][2];
+		Room* gameBoard[4][4];
 		int currentRow = 0;
 		int currentColumn = 0;
-
+		stack<pair<int, int>> lastRoom;
+		bool movedBack = false;
 		vector<pair<string, string>> roomVect;
 	public:
 		gameMap(){}
 		void addRoom(Room* aRoom);
+		void initializeMap();
 		void getNearbyRooms(int row, int column, Room* aRoom);
 		pair<int, int> getCoordinates();
 		void enterRoom(Room* aRoom);
@@ -46,7 +50,12 @@ namespace textAdventure{
 		void navigateBackward();
 		void chooseUserDirection(string userInput);
 		void setCurrentPosition(int curRow, int curColumn);
-		Room* roomAtPosition(int row, int col);
+		void rotateNegative90();
+		void rotatePositive90();
+		void printBoard();
+		pair<int, int> getCurrentPosition();
+		Room* roomAtPosition(int row, int column);
+		void goBack();
 		//void setMapSize(int row, int column);
 		
 	};
