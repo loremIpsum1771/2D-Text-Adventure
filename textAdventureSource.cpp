416983d8c5f378b718ffb1090700002798eed963
loremIpsum1771 <loremIpsum1771@users.noreply.github.com>
2015-08-12 18:01:41 -0400

    Update textAdventureSource.cpp

--- textAdventureSource.cpp
+++ textAdventureSource.cpp
@@ -1,7 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include "actors.h"
-#include "gameboard.h"
-#include "inventory.h"
+#include "actors.h"// header for main actor, enemy, weapons, healthpacks, and inventory classes
+#include "gameboard.h" //header for gameboard  and room objects
+#include "combatSimulation.h"// header for combat simulation and random number generator
 #include<iostream>
 #include<vector>
 #include<string>
@@ -11,6 +11,8 @@
 using namespace std;
 using namespace textAdventure;
 
+
+//splits each line from the file based on a specified delimiter and fills in the passed in vector which is parsed in the render map function
 void tokenizer(char str[], vector<string>& tokens, const char delim[]) {
 	tokens.clear();
 	char* tok = strtok(str, delim);
@@ -20,97 +22,153 @@ void tokenizer(char str[], vector<string>& tokens, const char delim[]) {
 		tok = strtok(nullptr, delim);
 	}
 }
-
+//creates the map for the game and constructs the enemy and pickup (i.e. the weapons and health packs that can be picked up) objects
 void renderMap(gameMap& map){
-	ifstream txtFile("adventureExposition.txt");
+	ifstream txtFile("gameSetUp.txt");
 	if (!txtFile) { cerr << "File not found"; return; }
 	string fileLine;
 	//getline(txtFile, fileLine);
 	int i = 0;
 	vector<Room*> roomPtrs;
 	vector<enemy*> enemyPtrs;
+	vector<healthPack*> hpPtrs;
+	vector<Weapon*> weaponPtrs;
 	while (getline(txtFile, fileLine)) {
-		if (i < 16){
-			vector<string> roomInfo;
-			vector<string> enemyInfo;
-			char delim[] = "|";
-			char delim2[] = "$";
-			char* charStopLine = new char[fileLine.size() + 1];
+		//# file lines = 20
+		if (i < 20){
+			vector<string> roomInfo; //vector for room info to be parsed
+			vector<string> enemyInfo; // vector for enemy info to be parsed
+			char* charStopLine = new char[fileLine.size() + 1]; //container for each file line
 			strcpy(charStopLine, fileLine.c_str());
+			//the room info extends from line 1 to line 16 (i lags 1 unit behind the line number)
+			if (i < 16){
+				//this branch is used exlusively to parse all of the room info
+				char delim[] = "|";
+				tokenizer(charStopLine, roomInfo, delim);
+				string roomName = *(roomInfo.begin());
+				string roomDescription = *(roomInfo.begin() + 1);
+				int roomXdim = stoi(*(roomInfo.begin() + 2));
+				int roomYdim = stoi(*(roomInfo.begin() + 3));
+				Room* roomPtr = new Room(roomName, roomXdim, roomYdim);
+				roomPtr->setRoomDesc(roomDescription);
+				map.addRoom(roomPtr);
+				if (i >= 13 && i < 16){ //used to hold pointers to the rooms where enemies will be positioned
+					roomPtrs.push_back(roomPtr);
+				}
 
-			tokenizer(charStopLine, roomInfo, delim);
-			tokenizer(charStopLine, enemyInfo, delim2);
-			string roomName = *(roomInfo.begin());
-			string roomDescription = *(roomInfo.begin() + 1);
-			int roomXdim = stoi(*(roomInfo.begin() + 2));
-			int roomYdim = stoi(*(roomInfo.begin() + 3));
-			Room* roomPtr = new Room(roomName, roomXdim, roomYdim);
+			}
+			if (i >= 16 && i < 18){
+				//this branch is used exlusively to parse all of the enemy info
+				char delim2[] = "$";
+				tokenizer(charStopLine, enemyInfo, delim2);
+				string enemyName = *(enemyInfo.begin());
+				int enemyHp = stoi(*(enemyInfo.begin() + 1));
+				int enemyStrength = stoi(*(enemyInfo.begin() + 2));
+				string weaponName = *(enemyInfo.begin() + 3);
+				enemy* enemyPtr = new enemy(enemyName);
+
+				Weapon* enemyWeapon = new Weapon(weaponName);
+				enemyPtr->setStrength(enemyHp);
+				//enemyPtr->decreaseHP(enemyHp);
+				enemyPtr->setWeapon(enemyWeapon);
+				enemyPtrs.push_back(enemyPtr);
+			}
+
+			if (i == 18){
+				//this branch is used exlusively to parse the info for the health pack that can be picked up
+				char delim3[] = "=";
+				vector<string> hpInfo;
+				tokenizer(charStopLine, hpInfo, delim3);
+				int hpStrength = stoi(*(hpInfo.begin() + 1));
+				healthPack* hpPtr = new healthPack(hpStrength);
+				hpPtrs.push_back(hpPtr);
+			}
+			if (i == 19){
+				//this branch is used exlusively to parse the info for the weapon that can be picked up
+				char delim4[] = "!";
+				vector<string> weaponInfo;
+				tokenizer(charStopLine, weaponInfo, delim4);
+				string weaponName = *(weaponInfo.begin());
+				Weapon* weaponPtr = new Weapon(weaponName);
+				weaponPtrs.push_back(weaponPtr);
+			}
 			
-			roomPtr->setRoomDesc(roomDescription);
-			
-
-			/*string enemyName = *(enemyInfo.begin());
-			int enemyHp = stoi(*(enemyInfo.begin() + 1));
-			int enemyStrength = stoi(*(enemyInfo.begin() + 2));
-			string weaponName = *(enemyInfo.begin() + 3);
-			enemy* enemyPtr = new enemy(enemyName);
-
-			knife* enemyWeapon = new knife();
-			enemyPtr->setStrength(enemyStrength);
-			enemyPtr->setHp(enemyHp);
-			enemyPtr->setWeapon(enemyWeapon);
-			roomPtr->addRoomEnemy(enemyPtr);*/
-			//enemyPtrs.push_back(enemyPtr);
-
-			map.addRoom(roomPtr);
-			/*roomNames.push_back(*(roomInfo.begin()));
-			cout << "room Names " << *(roomNames.begin()) << endl;
-			roomDescriptions.push_back(*(roomInfo.begin()+1));
-			cout << "room Descriptions " << *(roomDescriptions.begin()) << endl;*/
-
 			delete[] charStopLine;
 		}
 		i++;
+
+	}
+		roomPtrs[2]->addRoomHP(hpPtrs[0]);//Positions the health pack from the file inside of the specified room
+		roomPtrs[2]->addRoomWeapon(weaponPtrs[0]);//Positions the weapon from the file inside of the specified room
+		int j = 1;
+		for (int i = 0; i < roomPtrs.size()-1; i++, j--){//Positions the enemies in the intended rooms
+			roomPtrs[j]->addRoomEnemy(enemyPtrs[i]);
+
+		
+
 	}
 }
-
-
+//Main loop for the game
 void gameLoop(){
+	//char userControl = '0';
 	string userControl;
-
 	cout << "Enter Character name" << endl;
 	string cName;
 	cin >> cName;
-	mainActor anActor(cName);
-	gameMap map;
-	renderMap(map);
-
-	map.initializeMap();
-	map.printBoard();
 	
-	while (userControl != "quit"){
-		cout << "choose a direction" << endl;
+
+	gameMap map;
+	renderMap(map);//renders the gameMap and assigns it to the "map" instantiation
+	Weapon* weaponPtr1 = new Weapon("knife");//weapons that the player spawns with
+	Weapon* weaponPtr2 = new Weapon("bat");
+	/*vector<Weapon*> startWeapons;
+	startWeapons.push_back(weaponPtr1);
+	startWeapons.push_back(weaponPtr2);*/
+	inventory* startInventory = new inventory();//inventory that the player spawns with
+	startInventory->addWeapon(weaponPtr1);
+	startInventory->addWeapon(weaponPtr2);
+	mainActor* actorPtr = new mainActor(cName, startInventory);//initiallizes the player
+	//actorPtr->pickUpWeapon(startWeapons);
+	map.addGameUser(actorPtr);//spawns player inside of the map
+
+	map.initializeMap();//sets the users's start positions
+	map.printBoard();
+	//game loop menu
+	while (userControl != "q"){
+		cout << "choose a direction\n enter 'l' to go left, 'r' for right, 'f' to go forward, 'b' to go to the previous room" << endl;
 		cin >> userControl;
-		if (userControl == "left"){
+		if (userControl == "l"){
 			map.navigateLeft();
 		}
-		if (userControl == "right"){
+		if (userControl == "r"){
 			map.navigateRight();
-			map.printBoard();
-			
+			//map.printBoard();
+
 		}
-		if (userControl == "forward"){
+		if (userControl == "f"){
 			map.navigateForward();
 		}
-		if (userControl == "back"){
-		
+		if (userControl == "b"){
+
 			map.navigateBackward();
 		}
+		/*switch (userControl){
+		case 'l':
+			map.navigateLeft();
+		case 'r':
+			map.navigateRight();
+		case 'f':
+			map.navigateForward();
+		case 'b':
+			map.navigateBackward();
+		}*/
+
+		
 	}
 }
 
 int main(){
-	
+
 
 	gameLoop();
 	return 0;
