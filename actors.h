78e8d9669696d65c710ab3462d08a2ae3a6a4880
loremIpsum1771 <loremIpsum1771@users.noreply.github.com>
2015-08-12 17:59:08 -0400

    Update actors.h

--- actors.h
+++ actors.h
@@ -4,54 +4,109 @@
 #include<vector>
 #include<string>
 #include<iostream>
-#include "inventory.h"
+#include<map>
 
-using namespace std;
 namespace textAdventure{
-	class inventory;
-	class weapon;
-	class healthPack;
+	//class inventory;
+	//class Weapon;
+	//class healthPack;
+	
+	class katana{
+		int strengthPoints = 10;
+	public:
+		katana(){}
+		int getStrength();
+
+	};
+
+	class Weapon{
+		int damagePoints = 0;
+		std::string weaponName;
+	public:
+		Weapon(const std::string& wName);
+		std::string useWeapon();
+		int getWeaponStrength();
+		std::string getName();
+		
+
+	};
+	class healthPack{
+		int hpStrength = 0;
+	public:
+		healthPack(int hp);
+		int getHealthValue();
+		//void addHealth(mainActor* anActor);
+	};
+	
+	class inventory{
+		typedef std::map<std::string, Weapon*> WeaponsContainer;
+		typedef std::pair<std::string, Weapon*> MyPair;
+		WeaponsContainer weapons;
+		std::vector<healthPack*> healthPacks;
+		//map<int, hints> hintsContainer;
+
+
+	public:
+		void discardHealthPack(healthPack* hp);
+		void addHealthPack(healthPack* anHp);
+		void addWeapon(Weapon* aWeapon);
+		void displayWeapons();
+		void displayhealthPacks();
+		std::vector<healthPack*> getHealthPacks();
+		Weapon* selectWeapon(std::string weaponName);
+	};
 	class enemy{
-		string enemyName;
+		std::string enemyName;
 		double battleRatio;
 		int currentHP;
 		//int effectivness;
 		int enemyStrength;
-		weapon* enemyWeapon;
+		Weapon* enemyWeapon;
 
 	public:
-		enemy(const string& eName);
-		string getEnemyName();
+		enemy(const std::string& eName);
+		std::string getEnemyName();
 		int getEnemyStrength();
-		void setStrength(int battleRatio); 
-		void setHp(int battleRatio);
-		void decreaseHP(int hp);
-		void setWeapon(weapon* aWeapon);
+		void setStrength(int battleRatio);
+		//void setHp(int battleRatio);
+		//void decreaseHP(int hp);
+		void setWeapon(Weapon* aWeapon);
+		int enemyWeaponStrength();
 		bool dead = false;
 
 	};
 	class mainActor{
-		string actorName;
+		std::string actorName;
 		int battleRatio = 0;
 		int currentHP = 100;
-		inventory* inventory;
+		inventory* userInventory;
 		//vector<weapon> Inventory;
 		int effectivness;
 	public:
-		mainActor(const string& nName);
-		string getActorName();
+		mainActor(const std::string& nName , inventory* startInventory);
+		std::string getActorName();
+		void destroyHP(healthPack* hp);
 		void setStrength(int battleRatio);
 		void changeHP(int hp);
-		void addHealthPoints(int hp);
+		//void addHealthPoints(int hp);
 		//void addStrength(int str);
-		int getActorStrength();
-		void addWeapon(weapon* aWeapon);
-		void addHealthPack(healthPack* hp);
+		int getActorHealth();
+		//void addWeapon(Weapon* aWeapon);
+		void pickUpHP(healthPack* hp);
 		void displayInventory();
-		void battle(enemy& nobleOpponent);
+		void pickUpWeapon(Weapon* weapon);
+		void applyHealth();
+		Weapon* selectWeapon(std::string weaponName);
 		bool dead = false;
 
 	};
+
+
+
+
+	
+
+	
 }
 
 
